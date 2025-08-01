257ed7049557f9541212f04edbd99649aa21280a
ManjunathD625 <manjunath.d@numocity.com>
2025-08-01 14:30:56 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import "./assets/css/index.css";
-import Experience from "./pages/Experience/Experience";
-import Contact from "./pages/Contact/Contact";
-import Projects from "./pages/Projects/Projects";
 import Header from "./pages/Header/Header";
-import Hero from "./pages/Hero/Hero";
-import Skills from "./pages/Skills/Skills";
-import Education from "./pages/Education/Education";
+
+// Lazy-loaded components
+const Hero = lazy(() => import("./pages/Hero/Hero"));
+const Skills = lazy(() => import("./pages/Skills/Skills"));
+const Experience = lazy(() => import("./pages/Experience/Experience"));
+const Education = lazy(() => import("./pages/Education/Education"));
+const Contact = lazy(() => import("./pages/Contact/Contact"));
+const Projects = lazy(() => import("./pages/Projects/Projects"));
 
 import { Route, Routes } from "react-router-dom";
 
@@ -17,26 +19,26 @@ export default function App() {
     <>
       <Header />
       {/* Conditional Rendering */}
-      {isOnePage ? (
-        // One-Page Mode: Render all components together
-        <>
-          <Hero />
-          <Skills />
-          <Experience />
-          <Education />
-          <Contact />
-        </>
-      ) : (
-        // Router Mode: Use routes for navigation
-        <Routes>`
-          <Route path="/" element={<Hero />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        {isOnePage ? (
+          <>
+            <Hero />
+            <Skills />
+            <Experience />
+            <Education />
+            <Contact />
+          </>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        )}
+      </Suspense>
     </>
   );
 }
